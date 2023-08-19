887e15d03614ebbfcffc3f0a71779ddd0fc5ab0c
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-08-19 21:32:09 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,12 +5,7 @@ using namespace std;
 
 #define FOR(_i,_a,_b) for(int _i = (_a); _i < (_b); _i++)
 #define pb push_back
-
-//order-set
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+typedef long long ll;
 
 int main()
 {
