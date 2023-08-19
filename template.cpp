b46cfc41658ceda976f94500ba0b10b40698e819
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-08-19 21:21:12 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,12 @@
+//https://www.onlinegdb.com/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(_i,_a,_b) for(int _i = (_a); _i < (_b); _i++)
+#define pb push_back
+
+int main()
+{
+  return 0;
+}
