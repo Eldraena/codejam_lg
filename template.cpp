c5f1c0a1cb6f250798edee050e19c0bdac95571d
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-08-19 21:45:29 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define fio ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define FOR(_i,_a,_b) for(int _i = (_a); _i < (_b); _i++)
 #define pb push_back
+#define endl "\n"
 typedef long long ll;
 
 int main()
