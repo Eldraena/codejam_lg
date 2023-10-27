f482280e88dff7c9c9d8b3d0abb0b23046228622
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-10-27 22:33:15 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,22 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define fio ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define pb push_back
-#define endl "\n"
-typedef long long ll;
-ll LL_MAX = 1e18, LL_MIN = -1e18;
-
-int main()
-{
-    fio;
-
-    int t;
-    cin >> t;
-    while(t--)
-    {
-
-    }
-  
-    return 0;
-}
+	"include":
+	{
+		"prefix":"cpinit"
+		"body":
+		[
+			"#include <bits/stdc++.h>",
+			"using namespace std;",
+			"",
+			"#define fio ios_base::sync_with_stdio(false); cin.tie(NULL)",
+			"#define int long long int",
+			"#define F first",
+			"#define S second",
+			"#define pb push_back",
+			"#define endl \"\\n\"",
+			"",
+			"int32_t main()",
+			"{",
+			"   fio;",
+			"",
+			"#ifndef ONLINE_JUDGE",
+			"   freopen(\"input.txt\", \"r\", stdin);",
+			"   freopen(\"output.txt\", \"w\", stdout);",
+			"#endif",
+			"",
+			"   int t;",
+			"   cin >> t;",
+			"   while(t--)",
+			"   {",
+			"",
+			"   }",
+			"",
+			"   return 0;",
+			"}",
+		]
+		"description":"cpinit"
+	}
